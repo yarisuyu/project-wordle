44d809aec68f00fcf1bc53e9c388690bfc3fbcc4
Elena Oblomova <lisinaeg@gmail.com>
2024-10-05 14:05:53 +0300

    Add the form for guess input

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState('');
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={event => {
+        event.preventDefault();
+        console.log("Your guess is:", guess);
+        setGuess("");
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        minLength="5"
+        maxLength="5"
+        pattern="[A-Za-z]{5}"
+        title="The length of the word MUST be 5"
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={event => setGuess(event.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
